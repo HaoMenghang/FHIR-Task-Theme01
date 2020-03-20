310b6e9c2021d1840a0773d648826e522249c1b3
HaoMenghang <43610654+HaoMenghang@users.noreply.github.com>
2020-03-20 03:21:31 +0000

    theme01

---
+++ FHIRworks_2020-master/FHIRworks_2020-master/dotnet-azure-fhir-web-api/Controllers/PatientController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using HDR_UK_Web_Application.IServices;
+
+namespace HDR_UK_Web_Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+
+        private readonly IPatientService _service;
+
+        public PatientController(IPatientService service)
+        {
+            _service = service;
+        }
+
+        // GET: api/Patient/
+        [HttpGet("GetPatients")]
+        public async Task<List<JObject>> GetPatients()
+        {
+            return await _service.GetPatients();
+        }
+
+        // GET: api/Patient/<patient ID>
+        //[HttpGet("{id}", Name = "GetPatient")]
+        [HttpGet("GetPatient")]
+        public async Task<JObject> GetPatient([FromQuery]string id)
+        {
+            return await _service.GetPatient(id);
+        }
+
+        // GET: api/Patient/pages/<number of pages>
+        [HttpGet("GetPatientPages")]
+        public async Task<List<JObject>> GetPatientPages([FromQuery]int pages)
+        {
+            return await _service.GetPatientPages(pages);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FHIR-Task-Theme01
+My project is: Generating letters, forms and documents prefilled from FHIR records (Word and PDF).
+
+My solution is by searching for user‘s own patient id, the patient can query his or her  personal information and some last observation data in two forms. Besides, patients can choose to download the forms to their computer in pdf format.
+
+The code uses C #. The main function is to get data in the back-end data interface. The method is to analyze the jason file and then send the http request and accept the response. For example, when a patient enters his id, the first form will obtain the patient's information through the interface the teacher given. Then I use the patient's id to get all the historical data of the patient's observation in the hospital, take out the first piece of data and load it into the second table. So in total, there are three interfaces are requested.
